e6e30777d1c73a7b8b7d534cc46a8a7dbd03bcb0
RennanD <rplaykl@gmail.com>
2022-11-09 15:26:15 -0300

    feat: configure tailwind

---
+++ tailwind.config.cjs
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.tsx'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: 'Roboto, sans-serif',
+      },
+      colors: {
+        gray: {
+          100: '#E1E1E6',
+          200: '#C4C4CC',
+          300: '#7C7C8A',
+          400: '#323238',
+          500: '#29292E',
+          600: '#202024',
+          700: '#121214',
+        },
+        green: {
+          500: '#00B37E',
+          700: '#00875F',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
